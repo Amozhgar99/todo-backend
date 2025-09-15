9bc88639a82b0cdd7a1164c5e8c98ef96657eab2
Amozhgar <amozhgar.saadi@lezzoo.com>
2025-09-15 18:47:58 +0300

    fix it

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,7 +3,7 @@ const Todo = require("../models/Todo");
 
 const router = express.Router();
 
-router.get("/", async (res) => {
+router.get("/", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
