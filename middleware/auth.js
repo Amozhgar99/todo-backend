155e527c2c66e2907e221f8a013e0d7dc1ac249e
Amozhgar <amozhgar.saadi@lezzoo.com>
2025-11-23 16:20:58 +0300

    fix: removed comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ function auth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.id; // attach userId
+    req.user = decoded.id;
     next();
   } catch (err) {
     res.status(401).json({ error: "Token is not valid" });
